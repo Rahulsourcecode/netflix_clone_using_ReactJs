bde1524f82e63e86c8fd3224d57c0f4667eb6b42
Rahul R <rajeshrahul831@gmail.com>
2023-05-05 19:17:42 +0530

    second

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import NavBar from './Components/Navbar/NavBar';
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
-
+import {action, originals,comedy} from './urls'
 function App() {
   return (
     <div className="App">
      <NavBar></NavBar>
      <Banner></Banner>
-     <RowPost></RowPost>
+     <RowPost url={originals} title='Netflix Originals'></RowPost>
+     <RowPost url={action} title='Action' isSmall></RowPost>
+     <RowPost url={comedy} title='comedy' isSmall></RowPost>
+     <RowPost url={comedy} title='comedy' isSmall></RowPost>
+
     </div>
   );
 }
