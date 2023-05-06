7b7111aa55b985ad49199096bc0f12c5e903b864
Rahul R <rajeshrahul831@gmail.com>
2023-05-06 10:13:29 +0530

    third

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import NavBar from './Components/Navbar/NavBar';
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
-import {action, originals,comedy} from './urls'
+import Footer from './Components/Footer/Footer';
+import {action, originals,comedy,horror,romantic, documentary} from './urls'
 function App() {
   return (
     <div className="App">
@@ -11,8 +12,10 @@ function App() {
      <RowPost url={originals} title='Netflix Originals'></RowPost>
      <RowPost url={action} title='Action' isSmall></RowPost>
      <RowPost url={comedy} title='comedy' isSmall></RowPost>
-     <RowPost url={comedy} title='comedy' isSmall></RowPost>
-
+     <RowPost url={horror} title='horror' isSmall></RowPost>
+     <RowPost url={romantic} title='Romantic' isSmall></RowPost>
+     <RowPost url={documentary} title='Documentary' isSmall></RowPost>
+     <Footer></Footer>
     </div>
   );
 }
